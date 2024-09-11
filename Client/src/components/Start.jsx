ed38238375f956fa18b2633cc916546e721fa6c4
Amplejohnny <uzunuel@gmail.com>
2024-09-11 09:04:07 +0100

    Testing

--- Client/src/components/Start.jsx
+++ Client/src/components/Start.jsx
@@ -9,7 +9,7 @@ const Start = () => {
   axios.defaults.withCredentials = true;
   useEffect(() => {
     axios
-      .get("https://employee-ms-api.vercel.app/verify")
+      .get("https://employee-ms-api.vercel.app/auth/verify")
       .then((result) => {
         if (result.data.Status) {
           if (result.data.role === "admin") {
--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { adminRouter } from "./Routes/adminRoute.js";
 import { employeeRouter } from "./Routes/employeeRoute.js";
-import Jwt from "jsonwebtoken";
+import { verifyRouter } from "./Routes/verifyRoute.js";
 import cookieParser from "cookie-parser";
 
 import dotenv from "dotenv";
@@ -23,24 +23,11 @@ app.use(cookieParser());
 
 app.use("/auth", adminRouter);
 app.use("/auth", employeeRouter);
+app.use("/auth", verifyRouter);
 app.use(express.static("public"));
 
-const verifyUser = (req, res, next) => {
-  const token = req.cookies.token;
-  if (token) {
-    Jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, decoded) => {
-      if (err) return res.json({ Status: false, Error: "Invalid Token" });
-      req.id = decoded.id;
-      req.role = decoded.role;
-      next();
-    });
-  } else {
-    return res.json({ Status: false, Error: "Not autheticated" });
-  }
-};
-
-app.get("/verify", verifyUser, (req, res) => {
-  return res.json({ Status: true, role: req.role, id: req.id });
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
 app.listen(port, () => {
