fb33d257c8ce99f9dffd6265220cd1c6d3356467
Amplejohnny <uzunuel@gmail.com>
2024-09-13 17:31:17 +0100

    Debugging

--- Server/Routes/adminRoute.js
+++ Server/Routes/adminRoute.js
@@ -7,22 +7,22 @@ import path from "path";
 
 const router = express.Router();
 
-router.post("/adminregister", async (req, res) => {
-  const sql = `INSERT INTO admin (email, password) VALUES (?)`;
-  if (!req.body.password || !req.body.email) {
-    return res.json({ Status: false, Error: "Please fill all the fields" });
-  }
-  try {
-    const hash = await bcrypt.hash(req.body.password, 10);
-    db.query(sql, [[req.body.email, hash]], (err, result) => {
-      if (err) return res.json({ Status: false, Error: "Query Error" });
-      res.json({ Status: true });
-      res.redirect("/login");
-    });
-  } catch (err) {
-    return res.json({ Status: false, Error: "Error while hashing password" });
-  }
-});
+// router.post("/adminregister", async (req, res) => {
+//   const sql = `INSERT INTO admin (email, password) VALUES (?)`;
+//   if (!req.body.password || !req.body.email) {
+//     return res.json({ Status: false, Error: "Please fill all the fields" });
+//   }
+//   try {
+//     const hash = await bcrypt.hash(req.body.password, 10);
+//     db.query(sql, [[req.body.email, hash]], (err, result) => {
+//       if (err) return res.json({ Status: false, Error: "Query Error" });
+//       res.json({ Status: true });
+//       res.redirect("/login");
+//     });
+//   } catch (err) {
+//     return res.json({ Status: false, Error: "Error while hashing password" });
+//   }
+// });
 
 router.post("/admin_login", async (req, res) => {
   const { email, password } = req.body;
@@ -95,152 +95,152 @@ router.post("/admin_login", async (req, res) => {
 // });
 
 
-router.get("/category", (req, res) => {
-  const sql = "SELECT * FROM category";
-  db.query(sql, (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" });
-    return res.json({ Status: true, Result: result });
-  });
-});
+// router.get("/category", (req, res) => {
+//   const sql = "SELECT * FROM category";
+//   db.query(sql, (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" });
+//     return res.json({ Status: true, Result: result });
+//   });
+// });
 
-router.post("/add_category", (req, res) => {
-  const sql = "INSERT INTO category (`name`) VALUES (?)";
-  db.query(sql, [req.body.category], (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" });
-    return res.json({ Status: true });
-  });
-});
+// router.post("/add_category", (req, res) => {
+//   const sql = "INSERT INTO category (`name`) VALUES (?)";
+//   db.query(sql, [req.body.category], (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" });
+//     return res.json({ Status: true });
+//   });
+// });
 
-// image upload
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/Images");
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-const upload = multer({
-  storage: storage,
-});
+// // image upload
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "public/Images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(
+//       null,
+//       file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+//     );
+//   },
+// });
+// const upload = multer({
+//   storage: storage,
+// });
 
-router.post("/add_employee", upload.single("image"), (req, res) => {
-  const sql = `INSERT INTO employee 
-      (name, email, password, salary, address, category, image) 
-      VALUES (?)`;
+// router.post("/add_employee", upload.single("image"), (req, res) => {
+//   const sql = `INSERT INTO employee 
+//       (name, email, password, salary, address, category, image) 
+//       VALUES (?)`;
 
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      console.error("Password hashing error:", err);
-      return res.json({ Status: false, Error: "Query Error" });
-    }
-    const values = [
-      req.body.name,
-      req.body.email,
-      (req.body.password = hash),
-      req.body.salary,
-      req.body.address,
-      req.body.category,
-      req.file.filename,
-    ];
-    db.query(sql, [values], (err, result) => {
-      if (err) return res.json({ Status: false, Error: err });
-      return res.json({ Status: true, Data: result });
-    });
-  });
-});
+//   bcrypt.hash(req.body.password, 10, (err, hash) => {
+//     if (err) {
+//       console.error("Password hashing error:", err);
+//       return res.json({ Status: false, Error: "Query Error" });
+//     }
+//     const values = [
+//       req.body.name,
+//       req.body.email,
+//       (req.body.password = hash),
+//       req.body.salary,
+//       req.body.address,
+//       req.body.category,
+//       req.file.filename,
+//     ];
+//     db.query(sql, [values], (err, result) => {
+//       if (err) return res.json({ Status: false, Error: err });
+//       return res.json({ Status: true, Data: result });
+//     });
+//   });
+// });
 
-router.get("/employee", (req, res) => {
-  const sql = "SELECT * FROM employee";
-  db.query(sql, (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" });
-    return res.json({ Status: true, Result: result });
-  });
-});
+// router.get("/employee", (req, res) => {
+//   const sql = "SELECT * FROM employee";
+//   db.query(sql, (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" });
+//     return res.json({ Status: true, Result: result });
+//   });
+// });
 
-router.get("/employee/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = "SELECT * FROM employee WHERE id = ?";
-  db.query(sql, [id], (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" });
-    return res.json({ Status: true, Result: result });
-  });
-});
+// router.get("/employee/:id", (req, res) => {
+//   const id = req.params.id;
+//   const sql = "SELECT * FROM employee WHERE id = ?";
+//   db.query(sql, [id], (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" });
+//     return res.json({ Status: true, Result: result });
+//   });
+// });
 
-router.put("/edit_employee/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = `UPDATE employee 
-        set name = ?, email = ?, salary = ?, address = ?, category_id = ? 
-        Where id = ?`;
-  const values = [
-    req.body.name,
-    req.body.email,
-    req.body.salary,
-    req.body.address,
-    req.body.category_id,
-  ];
-  db.query(sql, [...values, id], (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" + err });
-    return res.json({ Status: true, Result: result });
-  });
-});
+// router.put("/edit_employee/:id", (req, res) => {
+//   const id = req.params.id;
+//   const sql = `UPDATE employee 
+//         set name = ?, email = ?, salary = ?, address = ?, category_id = ? 
+//         Where id = ?`;
+//   const values = [
+//     req.body.name,
+//     req.body.email,
+//     req.body.salary,
+//     req.body.address,
+//     req.body.category_id,
+//   ];
+//   db.query(sql, [...values, id], (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" + err });
+//     return res.json({ Status: true, Result: result });
+//   });
+// });
 
-router.delete("/delete_employee/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = "delete from employee where id = ?";
-  db.query(sql, [id], (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" + err });
-    return res.json({ Status: true, Result: result });
-  });
-});
+// router.delete("/delete_employee/:id", (req, res) => {
+//   const id = req.params.id;
+//   const sql = "delete from employee where id = ?";
+//   db.query(sql, [id], (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" + err });
+//     return res.json({ Status: true, Result: result });
+//   });
+// });
 
-router.get("/admin_count", (req, res) => {
-  const sql = "select count(id) as admin from admin";
-  db.query(sql, (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" + err });
-    return res.json({ Status: true, Result: result });
-  });
-});
+// router.get("/admin_count", (req, res) => {
+//   const sql = "select count(id) as admin from admin";
+//   db.query(sql, (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" + err });
+//     return res.json({ Status: true, Result: result });
+//   });
+// });
 
-router.get("/employee_count", (req, res) => {
-  const sql = "select count(id) as employee from employee";
-  db.query(sql, (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" + err });
-    return res.json({ Status: true, Result: result });
-  });
-});
+// router.get("/employee_count", (req, res) => {
+//   const sql = "select count(id) as employee from employee";
+//   db.query(sql, (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" + err });
+//     return res.json({ Status: true, Result: result });
+//   });
+// });
 
-router.get("/salary_count", (req, res) => {
-  const sql = "select sum(salary) as salary from employee";
-  db.query(sql, (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" + err });
-    return res.json({ Status: true, Result: result });
-  });
-});
+// router.get("/salary_count", (req, res) => {
+//   const sql = "select sum(salary) as salary from employee";
+//   db.query(sql, (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" + err });
+//     return res.json({ Status: true, Result: result });
+//   });
+// });
 
-router.get("/admin_records", (req, res) => {
-  const sql = "select * from admin";
-  db.query(sql, (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" + err });
-    return res.json({ Status: true, Result: result });
-  });
-});
+// router.get("/admin_records", (req, res) => {
+//   const sql = "select * from admin";
+//   db.query(sql, (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" + err });
+//     return res.json({ Status: true, Result: result });
+//   });
+// });
 
-router.get("/admin_logout", (req, res) => {
-  res.clearCookie("token");
-  return res.json({ Status: true });
-});
+// router.get("/admin_logout", (req, res) => {
+//   res.clearCookie("token");
+//   return res.json({ Status: true });
+// });
 
-// Admin timestamp
-router.get("/timestamp", (req, res) => {
-  const sql = "SELECT created_at as time from admin";
-  db.query(sql, (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error" });
-    return res.json({ Status: true, Result: result });
-  });
-});
+// // Admin timestamp
+// router.get("/timestamp", (req, res) => {
+//   const sql = "SELECT created_at as time from admin";
+//   db.query(sql, (err, result) => {
+//     if (err) return res.json({ Status: false, Error: "Query Error" });
+//     return res.json({ Status: true, Result: result });
+//   });
+// });
 
 export { router as adminRouter };
--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { adminRouter } from "./Routes/adminRoute.js";
-import { employeeRouter } from "./Routes/employeeRoute.js";
-import { verifyRouter } from "./Routes/verifyRoute.js";
+import Jwt from "jsonwebtoken";
+// import { employeeRouter } from "./Routes/employeeRoute.js";
+// import { verifyRouter } from "./Routes/verifyRoute.js";
 import cookieParser from "cookie-parser";
 
 import dotenv from "dotenv";
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(
   cors({
-    origin: ["https://employee-ms-ui.vercel.app"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
@@ -22,9 +23,29 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/auth", adminRouter);
-app.use("/auth", employeeRouter);
-app.use("/auth", verifyRouter);
+// app.use("/auth", employeeRouter);
+// app.use("/auth", verifyRouter);
 app.use(express.static("public"));
+
+const router = express.Router();
+
+const verifyUser = (req, res, next) => {
+  const token = req.cookies.token;
+  if (token) {
+    Jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, decoded) => {
+      if (err) return res.json({ Status: false, Error: "Invalid Token" });
+      req.id = decoded.id;
+      req.role = decoded.role;
+      next();
+    });
+  } else {
+    return res.json({ Status: false, Error: "Not autheticated" });
+  }
+};
+
+router.get("/verify", verifyUser, (req, res) => {
+  return res.json({ Status: true, role: req.role, id: req.id });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
